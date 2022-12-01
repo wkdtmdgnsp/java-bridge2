0564a917e848c392e593ee22c97cc4c49a7ebb77
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2022-12-01 16:56:31 +0900

    feat(bridge) : 이동 기능 구현

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -3,6 +3,7 @@ package bridge.controller;
 import bridge.BridgeMaker;
 import bridge.BridgeNumberGenerator;
 import bridge.BridgeRandomNumberGenerator;
+import bridge.model.BridgeGame;
 import bridge.view.InputView;
 
 import java.util.ArrayList;
@@ -11,6 +12,7 @@ import java.util.List;
 public class BridgeController {
     private InputView inputView = new InputView();
     private List<String> bridge = new ArrayList<>();
+    private BridgeGame bridgeGame = new BridgeGame();
 
     public void run() {
         System.out.println("다리 건너기 게임을 시작합니다.\n");
@@ -33,6 +35,7 @@ public class BridgeController {
         try {
             System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
             String move = inputView.readMoving();
+            bridgeGame.move(bridge, move);
         } catch (IllegalArgumentException e) {
             move();
         }
--- src/main/java/bridge/model/BridgeGame.java
+++ src/main/java/bridge/model/BridgeGame.java
@@ -1,16 +1,47 @@
 package bridge.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+    private List<String> up = new ArrayList<>();
+    private List<String> down = new ArrayList<>();
+    private int count = 0;
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(List<String> bridge, String move) {
+        if (bridge.get(count).contentEquals("U")) {
+            moveUp(move);
+            return;
+        }
+        moveDown(move);
+    }
+
+    private void moveUp(String move) {
+        if (move.contentEquals("U")) {
+            up.add("O");
+            down.add(" ");
+            return;
+        }
+        up.add(" ");
+        down.add("X");
+    }
+
+    private void moveDown(String move) {
+        if (move.contentEquals("D")) {
+            up.add(" ");
+            down.add("O");
+            return;
+        }
+        up.add("X");
+        down.add(" ");
     }
 
     /**
