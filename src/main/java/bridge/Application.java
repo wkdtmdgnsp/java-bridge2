08faa0a998798dcfab28f3f00869893d1efa20f1
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2022-12-02 16:46:14 +0900

    초기화

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,11 +1,7 @@
 package bridge;
 
-import bridge.controller.BridgeController;
-
 public class Application {
 
     public static void main(String[] args) {
-        BridgeController bridgeController = new BridgeController();
-        bridgeController.run();
     }
 }
